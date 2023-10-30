ec23c39e64793290633c597f6026badd1dad3214
Victor Popa <victor.popa@draperanddash.onmicrosoft.com>
2023-10-30 16:16:22 +0000

    testing

--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -2,35 +2,39 @@ import pytest
 from dsptools.utils.execution import timeout
 import multiprocessing
 
+
 # Define a sample function for testing
 def sample_function():
     # Simulate a function that takes some time to execute
     import time
+
     time.sleep(2)
+
 
 # Test cases for the timeout decorator with on_timeout set to "skip"
 def test_timeout_skip_enabled():
     # Test with the decorator enabled and on_timeout set to "skip"
-    timeout_decorator = timeout(max_timeout=1, on_timeout='skip', enabled=True)
+    timeout_decorator = timeout(max_timeout=1, on_timeout="skip", enabled=True)
     decorated_function = timeout_decorator(sample_function)
 
     # The decorated function should not raise a TimeoutError
     decorated_function()
+
 
 def test_timeout_skip_disabled():
     # Test with the decorator disabled and on_timeout set to "skip"
-    timeout_decorator = timeout(max_timeout=1, on_timeout='skip', enabled=False)
+    timeout_decorator = timeout(max_timeout=1, on_timeout="skip", enabled=False)
     decorated_function = timeout_decorator(sample_function)
 
     # The decorated function should not raise a TimeoutError
     decorated_function()
+
 
 @pytest.mark.xfail(raises=TypeError, strict=True)
 def test_timeout_raise_enabled():
     # Test with the decorator disabled and on_timeout set to "skip"
-    timeout_decorator = timeout(max_timeout=1, on_timeout='raise', enabled=True)
+    timeout_decorator = timeout(max_timeout=1, on_timeout="raise", enabled=True)
     decorated_function = timeout_decorator(sample_function)
 
     # The decorated function should not raise a TimeoutError
     decorated_function()
-
